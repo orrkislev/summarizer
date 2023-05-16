706292847f07333ab601988ca8c018ac2648dea4
Orr Kislev <orrkislev@gmail.com>
2023-05-16 19:57:10 +0300

    .

--- components/NewReader.js
+++ components/NewReader.js
@@ -84,10 +84,8 @@ export default function NewReader(props) {
                     const iframe = document.querySelector('iframe')
                     if (!iframe) return;
                     const firstChild = iframe.contentDocument.body.children[0]
-                    if (firstChild.tagName === 'SECTION'){
-                        setMainDotPos({x:firstChild.offsetLeft, y:firstChild.offsetTop})
-                        setSummerizedParagraphs(getParagraphs(firstChild))
-                    }
+                    setMainDotPos({x:firstChild.offsetLeft, y:firstChild.offsetTop})
+                    setSummerizedParagraphs(getParagraphs(iframe.contentDocument.body))
                 }, 100)
             })
         }
@@ -104,41 +102,14 @@ export default function NewReader(props) {
         rendition.prev();
     }
 
-    // const clickDot = () => {
-    //     const index = summerizedParagraphs.findIndex(sp => sp.paragraph === currTarget)
-    //     if (index !== -1) {
-    //         const newSummerizedParagraphs = [...summerizedParagraphs]
-    //         newSummerizedParagraphs[index].visible = !newSummerizedParagraphs[index].visible
-    //         setSummerizedParagraphs(newSummerizedParagraphs)
-    //         return
-    //     } else {
-    //         setSummerizedParagraphs([...summerizedParagraphs, {
-    //             prevParagraphText: currTarget.previousSibling?.innerText,
-    //             paragraph: currTarget,
-    //             nextParagraphText: currTarget.nextSibling?.innerText,
-    //             visible: true
-    //         }])
-    //     }
-    // }
-
-    // const dotPosition = { x: -10, y: -10 }
-    // if (currTarget != null) {
-    //     dotPosition.x = currTarget.offsetLeft + bookRef.current.offsetLeft - 15
-    //     dotPosition.y = currTarget.offsetTop + bookRef.current.offsetTop + 15
-    // }
-
     const clickMainDot = () => {
-        // update all paragraphs to be visible
         const newSummerizedParagraphs = [...summerizedParagraphs]
         newSummerizedParagraphs.forEach(sp => sp.visible = true)
         setSummerizedParagraphs(newSummerizedParagraphs)
     }
 
-    console.log(mainDotPos)
-
     return (
         <div>
-            {/* <span style={{color:'white'}}>{debugText}</span> */}
             <TopButtons>
                 <TopButton onClick={prevPage}>PREV</TopButton>
                 <TopButton onClick={nextPage}>NEXT</TopButton>
@@ -151,7 +122,6 @@ export default function NewReader(props) {
                 return <SummerizedParagraphs key={index}
                     paragraph={sp.paragraph}
                     visible={sp.visible}
-                    // onMouseEnter={() => setCurrTarget(sp.paragraph)}
                     prev={sp.prevParagraphText}
                     next={sp.nextParagraphText}
                     offsetLeft={bookRef.current.offsetLeft}
